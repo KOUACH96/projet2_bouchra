55f378b6fa76c8eb8f70077d0c6cf706d6cb964c
Bouchra KOUACH <bouchra.kouach@sii-maroc.com>
2022-09-29 14:27:42 +0100

    projet2

---
+++ eclipse-workspace/projet1_bouchra/src/main/java/Bonjour.java
@@ -0,0 +1,5 @@
+public class Bonjour {
+    public static void main ( String[] args) {
+        System.out.println("Bonjour KOUACH et bienvenue chez java <3");
+        }
+}
---
+++ eclipse-workspace/projet1_bouchra/src/test/java/projet1_bouchra/TestBouchra.java
@@ -0,0 +1,13 @@
+package projet1_bouchra;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+class TestBouchra {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
